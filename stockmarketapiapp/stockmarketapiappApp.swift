fdf9700861bcc58a6c1841783d4a33b1d5de50a2
daniel.arghavanibadrabad27 <iyellowmousei@gmail.com>
2026-02-18 07:57:05 -0600

    Initial Commit

---
+++ stockmarketapiapp/stockmarketapiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  stockmarketapiappApp.swift
+//  stockmarketapiapp
+//
+//  Created by DANIEL ARGHAVANI BADRABAD on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct stockmarketapiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
