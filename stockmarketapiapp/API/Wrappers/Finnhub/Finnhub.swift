b8533534c74283231f4b36276715254a112d44f2
3XPL <iyellowmousei@gmail.com>
2026-02-23 08:50:04 -0600

    ps

---
+++ stockmarketapiapp/API/Wrappers/Finnhub/Finnhub.swift
@@ -0,0 +1,18 @@
+//
+//  Finnhub.swift
+//  stockmarketapiapp
+//
+//  Created by DANIEL ARGHAVANI BADRABAD on 2/23/26.
+//
+
+struct FinnhubAPI {
+    var API_KEY = "" // pls get pls
+    
+    func getCurrentStockPrice(name : String) -> Double {
+        return 0.0
+    }
+    
+    func searchStocks(name: String) -> [String] {
+        return []
+    }
+}
